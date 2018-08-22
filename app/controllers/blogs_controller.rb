e0becb24d5d4a4705862921ca008c1c679c682af
Takuya_Nakahara <yky2800.rthm.55tn@gmail.com>
2018-08-22 14:07:11 +0000

    Lesson by 18

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,5 +7,12 @@ class BlogsController < ApplicationController
   end
   
   def create
+    Blog.create(params.require(:blog).permit(:title, :content))
+    redirect_to new_blog_path
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
