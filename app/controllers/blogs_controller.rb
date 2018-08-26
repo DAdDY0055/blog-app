91012c3a1a443d45b175c6cdbaa8d91b170134b4
Takuya_Nakahara <yky2800.rthm.55tn@gmail.com>
2018-08-26 06:21:52 +0000

    Lesson by 31 Complete

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,16 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  
   def index
     @blogs = Blog.all
   end
   
   def new
-    @blog = Blog.new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
   end
   
   def create
@@ -17,15 +23,12 @@ class BlogsController < ApplicationController
   end
   
   def show
-    @blog = Blog.find(params[:id])
   end
   
   def edit
-    @blog = Blog.find(params[:id])
   end
   
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました!"
     else
@@ -33,8 +36,22 @@ class BlogsController < ApplicationController
     end
   end
   
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました!"
+  end
+  
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
+  end
+  
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
   get '/blogs', to: 'blogs#index'
   get '/stocks', to: 'stocks#index'
   resources :blogs
-  
 end
+
