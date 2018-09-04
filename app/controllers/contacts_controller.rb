7d0e57709af9f5b473277f22e465f086448d51dd
Takuya_Nakahara <yky2800.rthm.55tn@gmail.com>
2018-09-04 14:18:10 +0000

    make error_msg

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,17 @@ class ContactsController < ApplicationController
   end
   
   def create
-    Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content])
-    redirect_to new_contact_path
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content])
+      redirect_to new_contact_path, notice: "お問い合わせを投稿しました!"
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
   end
 end
