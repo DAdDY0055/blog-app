388e4642d368db60bdbc50bd07511eb236d56e90
Takuya_Nakahara <yky2800.rthm.55tn@gmail.com>
2018-09-05 05:29:33 +0000

    modify create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content])
+      #Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content])
       redirect_to new_contact_path, notice: "お問い合わせを投稿しました!"
     else
       render 'new'
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
   validates :content, length: { in: 1..140 }
 end
